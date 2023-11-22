db6d757ffeb5fd31fc1083675ba76503e8fb81d3
Marco Upia <markremembers@gmail.com>
2023-11-22 13:07:09 -0500

    soleved

---
+++ src/components/Filter.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Filter({ onCatergoryChange}) {
+    return (
+        <select name="filter" onChange={onCatergoryChange}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+        </select>
+    )
+}
+
+export default Filter;
